5989c133a02e6794db358056d5f5d9a5609d88ec
gaivanchi <37071300+gaivanchi@users.noreply.github.com>
2023-03-13 16:19:54 +0500

    Add game 'brain-progression'

---
+++ src/games/progression.js
@@ -0,0 +1,40 @@
+import {
+  showGreetingAndGetName, getRandomNumber, getRandomIndex, runStep, showCongratulation,
+} from '../index.js';
+
+const getProgressionArr = (firstItem, length, step) => {
+  const coll = [];
+
+  for (let i = firstItem; coll.length < length; i += step) {
+    coll.push(i);
+  }
+
+  return coll;
+};
+
+export default () => {
+  const userName = showGreetingAndGetName();
+
+  console.log('What number is missing in the progression?');
+
+  for (let countCorrectAnswers = 0; countCorrectAnswers < 3; countCorrectAnswers += 1) {
+    const firstItem = getRandomNumber(-50, 50);
+    const progressionStep = getRandomNumber(0, 5);
+    const progressionLength = getRandomNumber(5, 10);
+
+    const progressionArr = getProgressionArr(firstItem, progressionLength, progressionStep);
+    const hiddenIndex = getRandomIndex(progressionArr);
+    const correctAnswer = progressionArr[hiddenIndex];
+
+    progressionArr[hiddenIndex] = '..';
+    const question = `${progressionArr.join(' ')}`;
+
+    const result = runStep(question, correctAnswer, userName);
+
+    if (!result) {
+      return;
+    }
+  }
+
+  showCongratulation(userName);
+};
